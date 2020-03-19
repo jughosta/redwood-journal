cba752a5b937836e859fb3d8353614ae12f175b5
JuliaRechkunova <julia.rechkunova@gmail.com>
2020-03-19 23:01:39 +0100

    Update code style

--- web/src/index.js
+++ web/src/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
