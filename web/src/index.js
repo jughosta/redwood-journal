39a8c15f485043c6467990824801ec067bfbbeaf
JuliaRechkunova <julia.rechkunova@gmail.com>
2020-03-22 22:35:58 +0100

    Fix lint

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
-import { IdentityContextProvider } from 'react-netlify-identity-widget';
+import { IdentityContextProvider } from 'react-netlify-identity-widget'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 
 import Routes from 'src/Routes'
